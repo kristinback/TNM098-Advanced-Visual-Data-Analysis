43cadc60ce71f690daa7be5a3b201d0c98e1dbad
Kristin Bäck <kriba265@student.liu.se>
2017-04-25 09:58:58 +0200

    more skeleton

---
+++ Project/js/map.js
@@ -0,0 +1,13 @@
+function map(){
+
+	var idDiv = $("#map");
+
+	var margin = {top: 20, right: 20, bottom: 20, left: 20},
+    	width = idDiv.width() - margin.right - margin.left,
+        height = idDiv.height() - margin.top - margin.bottom;
+
+    
+
+
+	
+}
